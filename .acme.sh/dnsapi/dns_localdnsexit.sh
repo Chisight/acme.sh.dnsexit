de4b6f4631923115ee0a66830d82c612f04ab0fc
Chisight <11790570+Chisight@users.noreply.github.com>
2025-03-26 20:07:03 -0500

    update approach to finding the registered domain from the provided parameter.
    now loops through moving one element from the domain to the host per loop, trying to create the dns record each pass.
    initially moves _acme-challenge. and if present removes *. and does not attempt to create the dns record for these.

--- .acme.sh/dnsapi/dns_localdnsexit.sh
+++ .acme.sh/dnsapi/dns_localdnsexit.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 #run export DNSEXITAPIKEY=[your api key]
-#Usage: warning, this script will be sourced and then called as: dns_localdnsexit_add() _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+#Usage: warning, this script will be sourced and then called as: dns_localdnsexit_add _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 #where "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs" is the key that will be created under _acme-challenge.www.domain.com
 #later dns_localdnsexit_rm() will be called with the same parameters
 #warning, add creates new txt records, do not overwrite them as multiple records are needed.
@@ -12,27 +10,44 @@ dnsexit_Api="https://api.dnsexit.com/dns/"
 
 ########  Public functions #####################
 dns_localdnsexit_add() {
-  echo "echo i got here!!"
-  _info "info i got here!!"
-  sleep 20
   _info "Using localdnsexit" 
-  _debug "Command-line parameters:"$*""
+  _debug "Command-line parameters: $*"
+  # Initialize _sub_domain
+  _sub_domain=""
+  _domain=""
+
   fulldomain=$1
   txtvalue=$2
-  _debug "First detect the root zone"
-  if ! _get_root "$fulldomain"; then
-    _err "invalid domain"
-    return 1
+
+  # Strip off _acme-challenge.
+  if [[ $fulldomain == _acme-challenge.* ]]; then
+    _sub_domain=_acme-challenge.
+    _domain=${fulldomain#_acme-challenge.}
   fi
+
+  # Remove leading '*' if present
+  if [[ $_domain == \*.* ]]; then
+    _domain=${_domain#\*.}
+  fi  
+
   _debug "fulldomain=$fulldomain"
   _debug "txtvalue=$txtvalue"
   _debug _sub_domain "$_sub_domain"
   _debug _domain "$_domain"
 
-  _data=$( cat << EOF
+  # Loop to handle the _dnsexit_rest call
+  while true; do
+    # Check if there's still a period left
+    if [[ $_domain == \*.* ]]; then
+      _err "invalid domain"
+      return 1
+    fi
+
+    # Prepare the JSON data
+    _data=$(cat << EOF
 {
   "domain": "$_domain",
-  "add":{
+  "add": {
     "type": "TXT",
     "name": "$_sub_domain",
     "content": "$txtvalue",
@@ -42,36 +57,28 @@ dns_localdnsexit_add() {
 }
 EOF
 )
-  _debug "json request=" ${_data}
-  _dnsexit_rest ${_data}
 
-#  curl  -H "Content-Type: application/json" -H "apikey: $DNSEXITAPIKEY" --data @$HOME/acme.json https://api.dnsexit.com/dns/
+    _debug "json request= $_data"
+    
+    # Try the _dnsexit_rest call
+    if _dnsexit_rest "$_data"; then
+      break  # Success, exit the loop
+    fi
+    
+    # move the first part of the domain to _subdomain
+    _sub_domain=${_sub_domain}${_domain%%.*}
+    _domain=${_domain#*.}
 
-#use this instead of curl:
-#export _H1="Content-Type: application/json"
-#  _content="$(printf "*%s*\n" "$_content" | _json_encode)"
-#  _subject="$(printf "*%s*\n" "$_subject" | _json_encode)"
-#  _data="{\"token\": \"$PUSHOVER_TOKEN\",\"user\": \"$PUSHOVER_USER\",\"title\": \"$_subject\",\"message\": \"$_content\",\"sound\": \"$PUSHOVER_SOUND\", \"device\": \"$PUSHOVER_DEVICE\", \"priority\": \"$PUSHOVER_PRIORITY\"}"
-#  response="$(_post "$_data" "$PUSHOVER_URI")"
-
-
-
-  #rm ~/acme.json
+  done
 
   sleep 2m
+  return 0
 }
 
 #fulldomain txtvalue
 dns_localdnsexit_rm() {
   fulldomain=$1
   txtvalue=$2
-  _debug "First detect the root zone"
-  if ! _get_root "$fulldomain"; then
-    _err "invalid domain"
-    return 1
-  fi
-  _debug "fulldomain=$fulldomain"
-  _debug "txtvalue=$txtvalue"
   _debug _sub_domain "$_sub_domain"
   _debug _domain "$_domain"
 
@@ -88,35 +95,10 @@ EOF
   _debug "json request=" ${_data}
   _dnsexit_rest ${_data}
 
-
   return 0
 }
 
 ####################  Private functions below ##################################
-#_acme-challenge.www.domain.com
-#returns
-# _sub_domain=_acme-challenge.www
-# _domain=domain.com
-# _domain_id=sdjkglgdfewsdfg
-_get_root() {
-  fullname=$1
-  n=1
-  _sub_domain=$(echo $fullname | cut -d. -f-$n)
-  let n+=1
-  _domain=$(echo $fullname | cut -d. -f$n-)
-  while echo "$_domain"| nc whois.verisign-grs.com 43 | grep "No match for"; do 
-    _sub_domain=$(echo $fullname | cut -d. -f-$n)
-    let n+=1
-    _domain=$(echo $fullname | cut -d. -f$n-)
-    _debug $_sub_domain
-    _debug $_domain
-    if [ "$_domain" ]; then
-      return 1
-    fi
-  done 
-  return 0
-}
-
 _dnsexit_rest() {
   export _H1="Content-Type: application/json"
   export _H2="apikey: $DNSEXITAPIKEY"
